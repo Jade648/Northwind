7f1da86455128269c745128223dd68d990e2fd14
Jade 648 <maddiepeterson375@gmail.com>
2023-03-05 19:18:07 -0600

    Updated Home Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 public class HomeController : Controller
 {
+  // this controller depends on the BloggingRepository
+  private DataContext _dataContext;
+  public HomeController(DataContext db) => _dataContext = db;
+
   public IActionResult Index() => View();
+}
+
+public class ProductController : Category
+{
+  // this controller depends on the BloggingRepository
+  private DataContext _dataContext;
+  public ProductController(DataContext db) => _dataContext = db;
+
+  public IActionResult Index() => View(_dataContext.Product.OrderBy(p => p.CategoryName));
+
+    private IActionResult View(IOrderedQueryable<Category> categories)
+    {
+        throw new NotImplementedException();
+    }
 }
