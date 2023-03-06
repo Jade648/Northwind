eda8bcce55ce4150524ae20075119ef855d54be5
Jade 648 <maddiepeterson375@gmail.com>
2023-03-05 20:40:17 -0600

    Updated Category page

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-public class ProductController : Category
+public class ProductController : Controller
 {
 
     private DataContext _dataContext;
   public ProductController(DataContext db) => _dataContext = db;
 
-public IActionResult Index() => View(_dataContext.Northwind.OrderBy(c => c.Name));
+public IActionResult Category() => View(_dataContext.Product.OrderBy(p => p.CategoryName));
 
 }
 
