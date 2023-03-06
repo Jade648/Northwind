cd3a0b26ca5e22ad7dc5ff4609912128940bf2b6
Jade 648 <maddiepeterson375@gmail.com>
2023-03-06 09:06:21 -0600

    Updated Home Controller and Index and Layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 
+using Microsoft.AspNetCore.Mvc;
+
 public class HomeController : Controller
 {
     // this controller depends on the NorthwindRepository
     private DataContext _dataContext;
     public HomeController(DataContext db) => _dataContext = db;
-    public ActionResult Index() => View(_dataContext.Discounts);
+    public ActionResult Index() => View(_dataContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
 }
