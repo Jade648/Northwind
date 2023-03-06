1f5549d74b4fc32eb91ffacb4b91eeb7c418e51f
Jade 648 <maddiepeterson375@gmail.com>
2023-03-05 21:17:20 -0600

    Data Context and Migrations updated

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -5,6 +5,6 @@ public class DataContext : DbContext
   public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
   public DbSet<Category> Categories{ get; set; }
-  public DbSet<Product> Product { get; set; }
+  public DbSet<Product> Products { get; set; }
     public object Northwind { get; internal set; }
 }
