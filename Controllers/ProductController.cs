422478bfdb21a3e1e60dc193b60de8792fc4da5a
Madeline Peterson <maddiepeterson375@gmail.com>
2023-03-13 08:45:09 -0500

    Updated Product controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -9,13 +9,4 @@ public class ProductController : Controller
   public IActionResult Index(int id) => View(_dataContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
 }
 
-public class DiscountController : Controller
-{
-  private DataContext _dataContext;
-
-  public DiscountController(DataContext db)=>_dataContext = db;
-
-  public ActionResult Index() => View(_dataContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
-
-}
 
