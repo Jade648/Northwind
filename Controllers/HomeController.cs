f0be47a59548a0ba199dcaa771e0de568f36cc52
Madeline Peterson <maddiepeterson375@gmail.com>
2023-03-13 09:32:43 -0500

    Updated Home Controller file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,5 +5,5 @@ public class HomeController : Controller
     // this controller depends on the NorthwindRepository
     private DataContext _dataContext;
     public HomeController(DataContext db) => _dataContext = db;
-    public IActionResult Index() => View(_dataContext.Discounts);
+    public IActionResult Index() => View(_dataContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
 }
