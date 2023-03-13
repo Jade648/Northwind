6dc5fa4ffe0419b86b189e635c74bbb3e94ea50c
Jade 648 <maddiepeterson375@gmail.com>
2023-03-12 20:04:44 -0500

    Updated product controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -12,6 +12,8 @@ public class ProductController : Controller
 public class DiscountController : Controller{
   private DataContext _dataContext;
 
+  public DiscountController(DataContext db)=>_dataContext = db;
+
   public ActionResult Index() => View(_dataContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
 
 }
